659dcbc89aabadf483e48dd14b3fa4ed211de590
Ronald De Leon <A5ARMZZ@usac.mmm.com>
2017-05-04 23:23:28 -0700

    changed line 451

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.getElementsByClassName("randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.getElementsByClassName("randomPizzaContainer")[i], size);
-      var newwidth = (document.getElementsByClassName("randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
+    var x = document.getElementsByClassName("randomPizzaContainer");
+    for (var i = 0; i < x.length; i++) {
+      var dx = determineDx(x[i], size);
+      var newwidth = (x[i].offsetWidth + dx) + 'px';
+      x[i].style.width = newwidth;
     }
   }
 
